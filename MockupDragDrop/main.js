41bb21ffcfad6736c1895264d2c2c6ca2b0fa736
SofiaToivonen <sofia.toivonen@chalmers.se>
2018-11-20 14:42:17 +0100

    PDF updates

--- MockupDragDrop/main.js
+++ MockupDragDrop/main.js
@@ -184,26 +184,27 @@ function savePDF() {
     pdf.setFontSize(72);
     pdf.text(17, 40, "This is YOU.");
 
+    // Title font
+    pdf.setFontSize(20);
+
+    // Titles
+    pdf.text(17, 60, "Strengths");
+    pdf.text(17, 94, "Improve");
+    pdf.text(113, 94, "Tone down");
+
     pdf.setFontSize(12);
     pdf.setDrawColor("#bbbbbb");
 
-    // Strengths
-    pdf.text(17, 70, "My strengths");
-
     // Improve
-    pdf.text(17, 94, "Improve");
-    pdf.text(17, 120, "How are you to improve this?");
-    pdf.rect(17, 124, 86, 60, "S");
-
-    
+    pdf.text(17, 126, "How are you to improve this?");
+    pdf.rect(17, 130, 80, 80, "S");
 
     // Tone down
-    pdf.text(70, 150, "Tone down");
-    pdf.text(117, 150, "How are you to tone down this?");
-    pdf.rect(117, 154, 76, 60, "S");
+    pdf.text(113, 126, "How are you to tone down this?");
+    pdf.rect(113, 130, 80, 80, "S");
 
     // Job
-    pdf.text(17, 230, "What does your current job require?");
+    pdf.text(17, 230, "What characteristics does your current job require?");
     pdf.rect(17, 234, 176, 40, "S");
 
     pdf.setFontSize(12);
@@ -211,35 +212,40 @@ function savePDF() {
 
     // Improve words
     $('#zone1 li span').each(function(index, node) {
-        pdf.text(17, (102 + (6 * index)), node.textContent);
+        pdf.text(17, (102 + (6 * index)), (index + 1) + ". " + node.textContent);
+    });
+
+    // Tone down words
+    $('#zone2 li span').each(function(index, node) {
+        pdf.text(113, (102 + (6 * index)), (index + 1) + ". " + node.textContent);
     });
 
     // Strength words
     var strength = $('#zone3 li span');
     
-    //Print strength words
+    //Insert strength words
     if (strength[0] != undefined) {
-        pdf.text(17, 78, strength[0].textContent); 
+        pdf.text(17, 68, "1. " + strength[0].textContent); 
     }
 
     if (strength[1] != undefined) {
-        pdf.text(77, 78, strength[1].textContent); 
+        pdf.text(17, 74, "2. " + strength[1].textContent); 
     }
 
     if (strength[2] != undefined) {
-        pdf.text(137, 78, strength[2].textContent); 
+        pdf.text(65, 68, "3. " + strength[2].textContent); 
     }
 
     if (strength[3] != undefined) {
-        pdf.text(17, 84, strength[3].textContent); 
+        pdf.text(65, 74, "4. " + strength[3].textContent); 
     }
 
     if (strength[4] != undefined) {
-        pdf.text(77, 84, strength[3].textContent); 
+        pdf.text(113, 68, "5. " + strength[3].textContent); 
     }
 
     if (strength[5] != undefined) {
-        pdf.text(137, 84, strength[5].textContent); 
+        pdf.text(113, 74, "6. " + strength[5].textContent); 
     }
     //$('#zone3 li span').each(function(index, node) {
       //  pdf.text(17, (78 + (6 * index)), node.textContent);
@@ -248,11 +254,6 @@ function savePDF() {
     //$('#zone3 li span').each(function(index, node) {
     //    pdf.text(17, (78 + (6 * index)), node.textContent);
     //});    
-
-    // Tone down words
-    $('#zone2 li span').each(function(index, node) {
-        pdf.text(70, (158 + (6 * index)), node.textContent);
-    });
 
     pdf.save("characteristics.pdf");
 }
